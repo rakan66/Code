e52e1a4f74d976068decd11467b5f55776878f01
HP <HP@DESKTOP-IE3O570>
2021-02-05 13:10:40 +0300

    Fiaal Commit

--- Codee/src/PrintMyName.java
+++ Codee/src/PrintMyName.java
@@ -4,6 +4,7 @@ public class PrintMyName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("My name is Rakan");
+System.out.println("This is my last name Rakan");
 	}
 
 }
